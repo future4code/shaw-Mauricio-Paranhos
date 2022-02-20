bdd3b5bf26a77becd82bc0d90862e38b9d4d4f99
unknown <meparanhos@gmail.com>
2022-02-20 00:20:58 -0300

    projeto-lista-js

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -25,7 +25,6 @@ function calculaAreaRetangulo() {
   console.log(area)
 }
 
-
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
@@ -38,62 +37,87 @@ function imprimeIdade() {
 // EXERCÍCIO 03
 function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
+  imc = peso / (altura * altura)
 
+  return imc
 }
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
-
+  let nome = prompt("Digite seu nome")
+  nome.trim()
+  let idade = prompt("Digite sua idade")
+  idade.trim()
+  let email = prompt("Digite seu email")
+  email.trim()
+  const mensagem = `Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`
+  console.log(mensagem)
 }
 
 // EXERCÍCIO 05
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
-
+  let cor1 = prompt("Digite sua primeira cor favorita")
+  let cor2 = prompt("Digite sua segunda cor favorita")
+  let cor3 = prompt("Digite sua terceira cor favorita")
+  const listaDeCores = [cor1, cor2, cor3]
+  
+  console.log(listaDeCores)
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+    return string.toUpperCase()
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
-
+let qtdIngressos = custo / valorIngresso
+return qtdIngressos
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
-
+let tamStrings = string1.length === string2.length
+return tamStrings
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
-
+  const primeiroElemento = array[0]
+  return primeiroElemento
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
-
+//let numero = array.length
+return array[array.length - 1]
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
-
+  // let primeiro = array[array[0]]
+  // let ultimo = array[array.length - 1]
+  // const array2 = array.replaceAll(primeiro,ultimo)
+  // const array3 = array2.pop()
+  // return array2
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+primeiraPalavra = string1.toUpperCase()
+segundaPalavra = string2.toUpperCase()
 
+return primeiraPalavra === segundaPalavra
 }
 
 // EXERCÍCIO 13
