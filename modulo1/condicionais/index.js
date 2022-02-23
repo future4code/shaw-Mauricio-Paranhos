457e16de6cf0cdcb7013125802376fc7a78fd76c
unknown <meparanhos@gmail.com>
2022-02-22 23:36:18 -0300

    condicionais

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -101,3 +101,106 @@ if (generoMin === "fantasia") {
 } else {
     console.log("Escolha outro filme :(")
 }
+
+// Desafios
+
+// 1.
+
+const genero = prompt("Digite o gênero do filme")
+const preco = Number(prompt("Digite o valor do ingresso"))
+const generoMin = genero.toLowerCase()
+
+if (generoMin === "fantasia" && preco <= 15) {
+  const snack = prompt("Qual snack que você quer comprar?")
+  console.log("Bom filme!")
+  console.log("Aproveite o seu", snack)
+} else {
+    console.log("Escolha outro filme :(")
+}
+
+// 2.
+
+const nomeCompleto = prompt("Digite seu nome completo")
+const tipoJogo = prompt("Digite o tipo de jogo (IN para internacional e DO para doméstico)")
+const etapa = prompt("Digite a etapa do jogo (SF para semi-final, DT para decisão de terceiro lugar e FI para final")
+const categoria = Number(prompt("Digite a categoria (1, 2, 3 ou 4)"))
+const qtdIngressos = Number(prompt("Digite o número de ingressos"))
+const dolar = 4.10
+let valorIngresso = Number()
+
+switch (tipoJogo) {
+  case "IN":
+    if (etapa === "SF" && categoria === 1) {
+      valorIngresso = 1320
+    } else if (etapa === "SF" && categoria === 2){
+      valorIngresso = 880
+    } else if (etapa === "SF" && categoria === 3){
+      valorIngresso = 550
+    } else if (etapa === "SF" && categoria === 4){
+      valorIngresso = 220
+    } else if (etapa === "DT" && categoria === 1){
+      valorIngresso = 660
+    } else if (etapa === "DT" && categoria === 2){
+      valorIngresso = 440
+    } else if (etapa === "DT" && categoria === 3){
+      valorIngresso = 330
+    } else if (etapa === "DT" && categoria === 4){
+      valorIngresso = 170
+    } else if (etapa === "FI" && categoria === 1){
+      valorIngresso = 1980
+    } else if (etapa === "FI" && categoria === 2){
+      valorIngresso = 1320
+    } else if (etapa === "FI" && categoria === 3){
+      valorIngresso = 880
+    } else if (etapa === "FI" && categoria === 4){
+      valorIngresso = 330
+    }
+    console.log("---Dados da compra---")
+    console.log("Nome:", nomeCompleto)
+    console.log("Tipo de jogo:", tipoJogo)
+    console.log("Etapa do jogo:", etapa)
+    console.log("Categoria:", categoria)
+    console.log("Quantidade de ingressos:", qtdIngressos)
+    console.log("---Valores---")
+    console.log("Valor do ingresso:", valorIngresso)
+    console.log("Valor total:", qtdIngressos * (valorIngresso * dolar))
+    break;
+  case "DO":
+    if (etapa === "SF" && categoria === 1) {
+      valorIngresso = 1320
+    } else if (etapa === "SF" && categoria === 2){
+      valorIngresso = 880
+    } else if (etapa === "SF" && categoria === 3){
+      valorIngresso = 550
+    } else if (etapa === "SF" && categoria === 4){
+      valorIngresso = 220
+    } else if (etapa === "DT" && categoria === 1){
+      valorIngresso = 660
+    } else if (etapa === "DT" && categoria === 2){
+      valorIngresso = 440
+    } else if (etapa === "DT" && categoria === 3){
+      valorIngresso = 330
+    } else if (etapa === "DT" && categoria === 4){
+      valorIngresso = 170
+    } else if (etapa === "FI" && categoria === 1){
+      valorIngresso = 1980
+    } else if (etapa === "FI" && categoria === 2){
+      valorIngresso = 1320
+    } else if (etapa === "FI" && categoria === 3){
+      valorIngresso = 880
+    } else if (etapa === "FI" && categoria === 4){
+      valorIngresso = 330
+    }
+    console.log("Nome:", nomeCompleto)
+    console.log("Tipo de jogo:", tipoJogo)
+    console.log("Etapa do jogo:", etapa)
+    console.log("Categoria:", categoria)
+    console.log("Quantidade de ingressos:", qtdIngressos)
+    console.log("Valor do ingresso:", valorIngresso)
+    console.log("Valor total:", qtdIngressos * valorIngresso)
+    break;
+
+  default:
+    console.log("Erro!")
+    break;
+}
