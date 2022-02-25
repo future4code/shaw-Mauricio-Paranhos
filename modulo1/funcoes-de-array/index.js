6946f41c71a140052a18c7ab48327c75b707dd44
unknown <meparanhos@gmail.com>
2022-02-24 23:22:10 -0300

    funcoes-de-array

--- modulo1/funcoes-de-array/index.js
+++ modulo1/funcoes-de-array/index.js
@@ -60,11 +60,28 @@ const pets = [
 
  const poodleArray = pets.filter((item, index, array) => {
     return item.raca == "Poodle"
- })
+ }).map((item)=> console.log("Você ganhou um cupom de desconto de 10% para tosar o/a", item.nome, "!"))
  
- const racaPoodle = poodleArray
- const nomePoodle = racaPoodle.map((item, index, array) => {
-     return item.nome
- })
+//  2. 
 
- console.log("Você ganhou um cupom de desconto de 10% para tosar o/a", nomePoodle, "!")
+const produtos = [
+   { nome: "Alface Lavada", categoria: "Hortifruti", preco: 2.5 },
+   { nome: "Guaraná 2l", categoria: "Bebidas", preco: 7.8 },
+   { nome: "Veja Multiuso", categoria: "Limpeza", preco: 12.6 },
+   { nome: "Dúzia de Banana", categoria: "Hortifruti", preco: 5.7 },
+   { nome: "Leite", categoria: "Bebidas", preco: 2.99 },
+   { nome: "Cândida", categoria: "Limpeza", preco: 3.30 },
+   { nome: "Detergente Ypê", categoria: "Limpeza", preco: 2.2 },
+   { nome: "Vinho Tinto", categoria: "Bebidas", preco: 55 },
+   { nome: "Berinjela kg", categoria: "Hortifruti", preco: 8.99 },
+   { nome: "Sabão em Pó Ypê", categoria: "Limpeza", preco: 10.80 }
+]
+
+// a) 
+
+const somenteNomes = produtos.map((produto, index, array) => {
+   return produto.nome
+})
+
+console.log(somenteNomes)
+
