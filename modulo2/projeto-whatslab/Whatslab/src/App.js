1f180ae5f23be7c390e743463cdaedb6823b1d81
unknown <meparanhos@gmail.com>
2022-03-28 16:02:37 -0300

    projeto-whatslab

--- modulo2/projeto-whatslab/Whatslab/src/App.js
+++ modulo2/projeto-whatslab/Whatslab/src/App.js
@@ -1,26 +1,90 @@
-import React, {Component} from 'react'
-import './SecaoComentario.css'
+import React from 'react';
+import styled from 'styled-components';
+import img from './img/Wallpaper.jpg';
 
-// export class SecaoComentario extends Component {
-// 	state = {
-// 		valorInputComentario: ''
-// 	}
+const AppContainer = styled.div`
+  border: 1px solid black;
+  background-color: lightgreen;
+  height: 99vh;
+  width: 40vw;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`
 
-// 	onChangeComentario = (event) => {
-// 	  console.log(event.target.value)
-//     this.setState({valorInputComentario: event.target.value})
-// 	}
+const MensagensContainer = styled.div`
+  background-image: url(${img});
+  flex-grow: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+  justify-content: end;
+`
 
-// 	render() {
-// 		return <div className={'comment-container'}>
-// 			<input
-// 				className={'input-comentario'}
-// 				placeholder={'Comentário'}
-// 				value={this.state.valorInputComentario}
-// 				onChange={this.onChangeComentario}
-// 			/>
-// 			<button onClick={this.props.aoEnviar}>Enviar</button>
-// 		</div>
-// 	}
-// }
+const InputsContainer = styled.div`
+  display: flex;
+`
 
+const NomeImput = styled.input`
+  width: 100px;
+`
+
+const MensagemImput = styled.input`
+  flex-grow: 1;
+`
+
+class App extends React.Component {
+  state = {
+    mensagens: [],
+    usuarioValue: '',
+    mensagemValue: ''
+  }
+
+  onChangeUsuarioValue = (event) => {
+    this.setState({usuarioValue: event.target.value})
+  }
+
+  onChangeMensagemValue = (event) => {
+    this.setState({mensagemValue: event.target.value})
+  }
+
+  enviarMensagem = () => {
+    const novaMensagem = {
+      usuario: this.state.usuarioValue,
+      texto: this.state.mensagemValue
+    }
+
+    const novasmensagensArray = [novaMensagem, ...this.state.mensagens]
+
+    this.setState({mensagens: novasmensagensArray})
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MensagensContainer>
+          {this.state.mensagens.map((mensagem) => {
+            return <p>
+              <strong>{mensagem.usuario}</strong>: {mensagem.texto}
+            </p>
+          })}
+        </MensagensContainer>
+        <InputsContainer>
+          <NomeImput
+            onChange={this.onChangeUsuarioValue} 
+            value={this.state.usuarioValue} 
+            placeholder={'Nome'}
+          />
+          <MensagemImput
+            onChange={this.onChangeMensagemValue} 
+            value={this.state.mensagemValue} 
+            placeholder={'Mensagem'}
+          />
+          <button onClick={this.enviarMensagem}>Enviar</button>
+        </InputsContainer>
+      </AppContainer>
+    );
+  }
+}
+
+export default App;
