78e2bc476365938914c2ad7d977f91e07bbde87c
unknown <meparanhos@gmail.com>
2022-04-06 17:00:30 -0300

    apis-labenusers-finalizado

--- modulo2/apis-labenusers/labenusers/src/App.js
+++ modulo2/apis-labenusers/labenusers/src/App.js
@@ -1,51 +1,39 @@
-import axios from "axios";
 import React from "react";
-
-const headers = {
-  headers: {
-    Authorization: "mauricio-paranhos-shaw"
-  }
-};
+import TelaCadastro from "./components/TelaCadastro";
+import TelaListaUsuarios from "./components/TelaListaUsuarios";
 
 export default class App extends React.Component {
-  
   state = {
-    usuarios: [],
-    inputName:  ""
-  };
-  
-  componentDidMount() {
-    //Pegar todas os usuário
-    this.getAllUsers();
+    telaAtual: "cadastro"
   }
-  getAllUsers() {
-    const url =
-      "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users";
-    //Aqui é onde a função de get é chamada
-    //No get passamos a url, e o headers
-    axios
-      .get(url, headers)
-      .then((res) => {
-        //Caso de tudo certo guarde os usuários no estado
-        console.log(res.data);
-        this.setState({
-          usuarios: res.data.name
-        });
-      })
-      .catch((err) => {
-        //Caso de erro exiba-o no console
-        // console.log(err.response.data);
-      });
+
+  escolheTela = () => {
+    switch (this.state.telaAtual){
+      case "cadastro":
+        return <TelaCadastro irParaLista={this.irParaLista}/>
+        break;
+      case "lista":
+        return <TelaListaUsuarios irParaCadastro={this.irParaCadastro}/>
+        break;
+      default:
+        return <div>Erro! Página não encontrada.</div>
+        break;
+    }
+  }
+
+  irParaCadastro = () => {
+    this.setState({telaAtual: "cadastro"})
+  }
+
+  irParaLista = () => {
+    this.setState({telaAtual: "lista"})
   }
   
-  render() {
-    const componentsUsuarios = this.state.usuarios.map((usuario) => {
-      return <li key={usuario.id}>{usuario.name}</li>;
-    });
+  render(){
     return (
       <div>
-        <ul>{componentsUsuarios}</ul>
+        {this.escolheTela()}
       </div>
-    );
+    )
   }
 }
