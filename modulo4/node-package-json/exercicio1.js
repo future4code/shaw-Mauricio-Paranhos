d3d6bc8093e7ed53868ea6ea80a9af4eb6f1c06a
unknown <meparanhos@gmail.com>
2022-05-25 22:53:17 -0300

    node-package-json

---
+++ modulo4/node-package-json/exercicio1.js
@@ -0,0 +1,7 @@
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos.`)
+
+console.log(`Olá, ${nome}! Você tem ${idade} anos. Em sete anos você terá ${idade+7}`)
--- modulo4/node-package-json/index.js
+++
@@ -1,5 +0,0 @@
-const nome = process.argv[2]
-const idade = Number(process.argv[3])
-
-
-const frase = `Olá, ${nome}! Você tem ${idade} anos.`
