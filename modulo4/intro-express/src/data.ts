e158447f04baac9a061a0cc06ac6ea64933b41dc
unknown <meparanhos@gmail.com>
2022-05-31 21:58:20 -0300

    intro-express

---
+++ modulo4/intro-express/src/data.ts
@@ -0,0 +1,118 @@
+export const users = [
+    {
+        id: "1",
+        name: "Leanne Graham", 
+        email: "Sincere@april.biz",
+        phone: "736803156442",
+        website: "hildegard.org",
+        posts: [
+            {
+                userId: "1",
+                id: "1",
+                title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+                body: "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+              },
+              {
+                userId: "1",
+                id: "2",
+                title: "qui est esse",
+                body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+              },
+              {
+                userId: "1",
+                id: "3",
+                title: "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+                body: "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+              },
+              {
+                userId: "1",
+                id: "4",
+                title: "eum et est occaecati",
+                body: "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+              },
+              {
+                userId: "1",
+                id: "5",
+                title: "nesciunt quas odio",
+                body: "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+              }
+        ]
+    },
+    {
+        id: "2",
+        name: "Ervin Howell",
+        email: "Shanna@melissa.tv",
+        phone: "10692659309125",
+        website: "anastasia.net",
+        posts:[
+            {
+                userId: "2",
+                id: "6",
+                title: "et ea vero quia laudantium autem",
+                body: "delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi"
+            },
+            {
+                userId: "2",
+                id: "7",
+                title: "in quibusdam tempore odit est dolorem",
+                body: "itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio"
+            }
+        ]
+    },
+    {
+        id: "3",
+        name: "Clementine Bauch",
+        email: "Nathan@yesenia.net",
+        phone: "14631234447",
+        website: "ramiro.info",
+        posts:[]
+    },
+    {
+        id: "4",
+        name: "Patricia Lebsack",
+        email: "Julianne.OConner@kory.org",
+        phone: "493170962367",
+        website: "kale.biz",
+        posts:[
+            {
+                userId: "4",
+                id: "8",
+                title: "ullam ut quidem id aut vel consequuntur",
+                body: "debitis eius sed quibusdam non quis consectetur vitae\nimpedit ut qui consequatur sed aut in\nquidem sit nostrum et maiores adipisci atque\nquaerat voluptatem adipisci repudiandae"
+            },
+            {
+                userId: "4",
+                id: "9",
+                title: "doloremque illum aliquid sunt",
+                body: "deserunt eos nobis asperiores et hic\nest debitis repellat molestiae optio\nnihil ratione ut eos beatae quibusdam distinctio maiores\nearum voluptates et aut adipisci ea maiores voluptas maxime"
+            },
+            {
+                userId: "4",
+                id: "10",
+                title: "qui explicabo molestiae dolorem",
+                body: "rerum ut et numquam laborum odit est sit\nid qui sint in\nquasi tenetur tempore aperiam et quaerat qui in\nrerum officiis sequi cumque quod"
+            },
+            {
+                userId: "4",
+                id: "11",
+                title: "magnam ut rerum iure",
+                body: "ea velit perferendis earum ut voluptatem voluptate itaque iusto\ntotam pariatur in\nnemo voluptatem voluptatem autem magni tempora minima in\nest distinctio qui assumenda accusamus dignissimos officia nesciunt nobis"
+            }
+        ]
+    },
+    {
+        id: "5",
+        name: "Chelsey Dietrich",
+        email: "Lucio_Hettinger@annie.ca",
+        phone: "2549541289892",
+        website: "demarco.info",
+        posts:[
+            {
+                userId: "5",
+                id: "12",
+                title: "non est facere",
+                body: "molestias id nostrum\nexcepturi molestiae dolore omnis repellendus quaerat saepe\nconsectetur iste quaerat tenetur asperiores accusamus ex ut\nnam quidem est ducimus sunt debitis saepe"
+            }
+        ]
+    }
+]
--- modulo4/intro-express/src/index.ts
+++ modulo4/intro-express/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express"
 import cors from "cors"
+import {users} from "./data"
 
 const app = express()
 
@@ -8,17 +9,66 @@ app.use(cors())
 
 app.get("/", (req, res) => {          
     res.send("Hello from Express")
-    console.log("Hello")
-})
-
-app.listen(3003, () => {
-    console.log("Servidor funcionando em http://localhost:3003.")
+    console.log("Hello world")
 })
 
 type usuario = {
-    id: string | number
+    id: string
     name: string
-    phone: number
     email: string
+    phone: string
     website:string
 }
+
+type posts = {
+    userId:string
+    id: string
+    title: string
+    body: string
+}
+
+app.get('/users', (req, res) => {
+
+    const usuarios = users.map((user) => {
+        return user;
+    })
+
+    const resultado = users.flat(1)
+
+    res.send(resultado);
+});
+
+app.get('/post', (req, res) => {
+
+    const postagens = users.map((user) => {
+        return user.posts
+    }).flat(1)
+
+    res.send(postagens);
+});
+
+app.get('/post/:userId', (req, res) => {
+
+    const userId = req.params.userId
+
+    const userPost = users.map((user) => {
+        return user.id === userId
+    })
+
+    res.send(userPost)
+})
+
+    // peguei os posts
+
+    // const tracks = playlists.map((playlist) => {
+    //     return playlist.tracks
+    // }).flat(1)
+
+    // peguei as tracks das playlists e juntei em um array
+
+    // res.send(postagens)
+// })
+
+app.listen(3003, () => {
+    console.log("Servidor tá rodando!")
+})
