20464c9d618f8908529d0662d3b3ddc73e790380
unknown <meparanhos@gmail.com>
2022-06-09 11:11:34 -0300

    Projeto-Sistema-Bancário

--- modulo4/Projeto-Sistema-Bancario/src/index.ts
+++ modulo4/Projeto-Sistema-Bancario/src/index.ts
@@ -1,11 +1,59 @@
-import express from "express"
+import express, { Express, Request, Response } from "express"
 import cors from "cors"
+import { accounts } from "./accounts"
 
-const app = express()
+const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.listen(3003, () => {
-    console.log("Server is running in http://localhost:3003")
+app.post("/users/create", (req:Request, res:Response)=>{
+    try {
+        const { name, CPF, dateOfBirthAsString } = req.body
+
+        const [day, month, year] = dateOfBirthAsString.split("/")
+
+        const dateOfBirth: Date = new Date(`${year}-${month}-${day}`)
+
+        const ageInMilliseconds: number = Date.now() - dateOfBirth.getTime()
+
+        const ageInYears: number = ageInMilliseconds / 1000 / 60 / 60 / 24 / 365
+
+        if (ageInYears <18) {
+            res.statusCode = 406
+            throw new Error("Idade deve ser maior que 18 anos")
+        }
+
+        accounts.push({
+            name,
+            CPF,
+            dateOfBirth,
+            balance: 0,
+            statement: []
+        })
+
+        res.status(201).send("Conta criada com sucesso!")
+    } catch (error) {
+        console.log(error)
+        res.send(error.message)        
+    }
 })
+
+app.get("/users/all", (req:Request, res:Response)=>{
+    try {
+
+        if (!accounts.length) {
+            res.statusCode = 404
+            throw new Error("Nenhuma conta encontrada")
+        }
+
+        res.status(200).send(accounts)
+    } catch (error) {
+        res.send(error.message)
+    }
+})
+
+app.listen(3003, ()=>{
+    console.log("Servidor está rodando na porta 3003.")
+})
+
