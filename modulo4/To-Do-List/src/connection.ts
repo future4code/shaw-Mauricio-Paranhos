76b273e8a91b25554e7b52f23f85dff287fccfaa
unknown <meparanhos@gmail.com>
2022-06-13 15:35:45 -0300

    To-Do-List

--- modulo4/To-Do-List/src/connection.ts
+++ modulo4/To-Do-List/src/connection.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv"
 
 dotenv.config();
 
-const connection = knex({ //estabelece conexão com banco de dados
+const connection = knex({
    client: "mysql",
    connection: {
       host: process.env.DB_HOST,
--- modulo4/To-Do-List/src/index.ts
+++ modulo4/To-Do-List/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express"
 import cors from "cors"
+import createUser from "./endpoints/createUser"
+import getUserById from "./endpoints/getUserById"
+import editUser from "./endpoints/editUser"
+import createTask from "./endpoints/createTask"
+import getTaskById from "./endpoints/getTaskById"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+
+app.put('/user',createUser)
+app.get('/user/:id',getUserById)
+app.post('/user/edit/:id',editUser)
+app.put('/task',createTask)
+app.get('/task/:id',getTaskById)
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003")
